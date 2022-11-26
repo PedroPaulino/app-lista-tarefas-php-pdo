d9a5e4cc5f38d9b6e331da8eaf8b7d9a86623741
Pedro_paulino <pedropaulinoeng@hotmail.com>
2022-11-26 14:08:47 -0300

    Adição README.md / query sql do banco de dados

---
+++ README.md
@@ -0,0 +1,20 @@
+# Nome do projeto
+<h1 align="center"> Projeto Lista de Tarefas </h1>
+
+## Descrição do Projeto
+<p align="center"> Esse é um projeto acadêmico criado através do curso Desenvolvimento Web Completo 2022 dos professores Jorge Sant Ana e Jamilton Damasceno </p>
+<p align="center"> Além do projeto inicial foi acrescentado a funcionalidade de consultar as tarefas concluídas  </p>
+
+## Abrir e rodar o projeto 
+
+<ul>
+    <li> Clonar ou baixar o repositório </li>
+    <li> Executar o código php_com_pdo.sql </li>
+</ul>
+
+### Features
+
+- [x] Nova tarefa
+- [x] Tarefas pendentes
+- [x] Tarefas concluídas (Feature adicional)
+- [x] Todas tarefas
---
+++ php_com_pdo.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26-Nov-2022 às 18:03
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `php_com_pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_status`
+--
+
+CREATE TABLE `tb_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_status`
+--
+
+INSERT INTO `tb_status` (`id`, `status`) VALUES
+(1, 'pendente'),
+(2, 'realizado');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_tarefas`
+--
+
+CREATE TABLE `tb_tarefas` (
+  `id` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL DEFAULT 1,
+  `tarefa` text NOT NULL,
+  `data_cadastrado` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_tarefas`
+--
+
+INSERT INTO `tb_tarefas` (`id`, `id_status`, `tarefa`, `data_cadastrado`) VALUES
+(1, 2, 'Pagar boleto faculdade dezembro', '2022-11-21 16:41:32'),
+(2, 2, 'Estudar PHP ', '2022-11-22 19:07:12'),
+(6, 1, 'Pagar financiamento da casa', '2022-11-26 01:20:11');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_status` (`id_status`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD CONSTRAINT `tb_tarefas_ibfk_1` FOREIGN KEY (`id_status`) REFERENCES `tb_status` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
